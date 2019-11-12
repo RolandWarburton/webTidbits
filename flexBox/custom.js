eb0a12a9d1f789535affb3d214f3a30386c3d80b
BuildTools <warburtonroland@gmail.com>
2019-11-12 23:14:44 +1100

    added flexbox examples

---
+++ flexBox/custom.js
@@ -0,0 +1,34 @@
+
+
+
+$( document ).ready(function() {
+  if (typeof jQuery == 'undefined') {alert("jQuery IS NOT loaded")}
+  else {
+    //alert("Jquery Loaded")
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
